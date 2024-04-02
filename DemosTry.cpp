2874fccf4b1325e4e167f0e1c4db6e52a6a86a27
kmarinovv <k.marinovv99@gmail.com>
2024-04-03 01:15:43 +0300

    last demo from vectors updated

--- DemosTry.cpp
+++ DemosTry.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cmath>
 #include <sstream>
+#include <algorithm>
+#include <list>
 
 // int main() {
 //     std::vector<double> squareRoots;
@@ -111,6 +113,83 @@
 
 //   return 0;
 // }
+
+//////////////fifth demo///////////////////////////
+
+// void print(const std::vector<int> &numbers) {
+//   for (int number : numbers) {
+//     std::cout << number << " ";
+//   }
+//   std::cout << std::endl << std::endl;
+// }
+
+// void findAndPrintElement(const std::vector<int> &numbers, int element) {
+//   std::cout << "Element: " << element << ' ';
+
+//   std::vector<int>::const_iterator it = std::find(numbers.begin(),numbers.end(), element);
+//   if (it == numbers.end()) {
+//     std::cout << "not found" << std::endl;
+//   } else {
+//     std::cout << "found at index: " << it - numbers.begin() << std::endl;
+//   }
+// }
+
+// int main() {
+//   std::vector<int> numbers { 1, -4, 13, 8 };
+//   findAndPrintElement(numbers, 7);
+//   findAndPrintElement(numbers, 8);
+
+//   std::sort(numbers.begin(), numbers.end());
+//   findAndPrintElement(numbers, 8);
+//   print(numbers);
+
+//   std::sort(numbers.begin(), numbers.end(), std::less<int>());
+//   findAndPrintElement(numbers, 8);
+//   print(numbers);
+
+//   std::sort(numbers.begin(), numbers.end(), std::greater<int>());
+//   findAndPrintElement(numbers, 8);
+//   print(numbers);
+
+//   return 0;
+// }
+
+
+
+int main() {
+    std::list<int> numbers {1, 3, 5};
+
+    // points to AFTER 5
+    std::list<int>::iterator lastItemIter = numbers.end();
+    lastItemIter--; // points to 5 now
+    numbers.insert(lastItemIter, 4);
+
+    // adds to the end - same as numbers.insert(numbers.end(), 6);
+    numbers.push_back(6);
+
+    std::list<int>::iterator secondItemIter = numbers.begin(); // points to 1
+    secondItemIter++; // points to 3 now
+    numbers.insert(secondItemIter, 2);
+
+    // inserts at start - same as numbers.insert(numbers.begin(), 0);
+    numbers.insert(numbers.begin(), 0);
+
+    // prints 0 1 2 3 4 5 6
+    for (int number : numbers) {
+        std::cout << number << " ";
+    }
+
+    // By the way, if you replace std::list with std::vector at the start,
+    // the printed result will be EXACTLY the same,
+    // except that you need to use insert instead of push_front
+    // (but operations will be slower, because vector insert is O(N), not O(1)).
+    // That's the point of iterators and data structures -
+    // they abstract-away the implementation details
+
+    return 0;
+}
+
+
 
 
 
